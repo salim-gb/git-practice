5fc1fc36aa101ded931c2ea950ef71aca7dc3983
salim-gb <salimalsado.gb@gmail.com>
2021-12-28 09:48:54 +0300

    added variable b in main activity

--- app/src/main/java/com/example/gitpracticeapp/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeapp/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val a = 6
+        val a = 7
+        val b = 7
     }
 }
