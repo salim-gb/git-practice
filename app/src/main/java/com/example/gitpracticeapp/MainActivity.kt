32a1772f7c4adfe3e0565e04361c2b3ead97e6f5
salim-gb <salimalsado.gb@gmail.com>
2021-12-28 10:06:02 +0300

    variable a changed in main activity

--- app/src/main/java/com/example/gitpracticeapp/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeapp/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val a = 7
+        val a = 9
         val b = 7
         val c = 9
         val y = 2
